0f6fdc6373e1931da52c4d8f553a12d57bfd09ec
Haley Rodzos <haley.rodzos@gmail.com>
2020-04-25 19:47:09 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+<<<<<<< HEAD
   get 'hello_world', to: 'static#hello_world'
+=======
+ get 'hello_world', to: 'static#about'
+ 
+>>>>>>> 834f67377edb2812f80c21b34962e5e8bedcd23f
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
